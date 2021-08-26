5e0fa1fbe0596c817fe10c57a4c178c9d42e17c6
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-08-26 11:50:39 -0700

    changed instance to local vars in controller methods

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,25 +1,25 @@
 class CatsController < ApplicationController
 
     def index
-        @cats = Cat.all
-        render json: @cats
+        cats = Cat.all
+        render json: cats
     end
 
     def create
-        @cat = Cat.create(cat_params)
-        render json: @cat
+        cat = Cat.create(cat_params)
+        render json: cat
     end
 
     def update
-        @cat = Cat.find(params[:id])
-        @cat.update(cat_params)
-        render json: @cat
+        cat = Cat.find(params[:id])
+        cat.update(cat_params)
+        render json: cat
     end
 
     def destroy
-        @cat = Cat.find(params[:id])
-        @cat.destroy
-        render json: @cat
+        cat = Cat.find(params[:id])
+        cat.destroy
+        render json: cat
     end
 
     private
