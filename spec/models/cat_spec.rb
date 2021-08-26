2d26774fe9d4d2b540d1349681de88f666204c71
galadoe <kgaladoe@gmail.com>
2021-08-26 15:39:32 -0700

    removed pending

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
 
   it 'should have a name' do
     cat = Cat.create(age: 2, enjoys: 'belly rubs and cuddle')
